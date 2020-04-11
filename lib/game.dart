03bc08cc2d369eac438d123a5306281586e1ed08
manqjayak <komangjaya453@gmail.com>
2020-04-11 14:58:28 +0800

    manipulasi layout game dan hasil. Project done!

--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pertanyaan.dart';
 import 'jawaban.dart';
+import 'hasil.dart';
 
 class Game extends StatefulWidget {
   String nama;
@@ -16,35 +17,74 @@ class _GameState extends State<Game> {
     {
       "soal": "Ayam apa yang bikin sebel?",
       "jawaban": [
-        "Ayamnya habis, tapi nasinya masih banyak",
-        "Ayam yang berkokok pagi hari"
+        {
+          "salah": "Ayamnya habis, tapi nasinya masih banyak",
+          "benar": "Ayam yang berkokok pagi hari"
+        },
+        {
+          "salah": "Ayam yang berkokok pagi hari",
+          "benar": "Ayamnya habis, tapi nasinya masih banyak"
+        }
       ]
     },
     {
       "soal": "Mengapa motor berhenti di depan lampu merah?",
-      "jawaban": ["Karena lampu merah", "Karena motornya direm"]
+      "jawaban": [
+        {
+          "salah": "Karena lampu merah",
+          "benar": "Karena motornya direm",
+        },
+        {
+          "salah": "Karena motornya direm",
+          "benar": "Karena lampu merah",
+        }
+      ]
     },
     {
       "soal": "Bahasa Mandari dari lantai basah?",
-      "jawaban": ["湿地板", "Lhi Chin"]
+      "jawaban": [
+        {
+          "salah": "湿地板",
+          "benar": "Lhi Chin",
+        },
+        {
+          "salah": "Lhi Chin",
+          "benar": "湿地板",
+        }
+      ]
     },
     {
       "soal": "Jenis kutu apa yang mengerikan?",
-      "jawaban": ["Kutu-kan", "Desiccants"]
+      "jawaban": [
+        {
+          "salah": "Kutu-kan",
+          "benar": "Desiccants",
+        },
+        {
+          "salah": "Desiccants",
+          "benar": "Kutu-kan",
+        }
+      ]
     },
     {
       "soal": "1 + 1 = ",
-      "jawaban": ['Jendela', 'dua']
+      "jawaban": [
+        {
+          "salah": 'Jendela',
+          "benar": 'dua',
+        },
+        {
+          "salah": 'dua',
+          "benar": 'Jendela',
+        }
+      ]
     }
   ];
 
-  Function _resetIndex() {
-    setState(() {
-      _index = 0;
-    });
-  }
-
-  void _lanjutPertanyaan() {
+  var _benar = [];
+  void _lanjutPertanyaan(String benar) {
+    _benar.add(benar);
+    print(_benar);
     setState(() {
       _index += 1;
     });
@@ -53,51 +93,61 @@ class _GameState extends State<Game> {
     }
   }
 
+  Function _resetSoal() {
+    setState(() {
+      _index = 0;
+      _benar.clear();
+    });
+  }
+
   @override
   // cara mengambil data nama = widget.nama
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          elevation: 0.0,
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-          ),
-          width: double.infinity,
-          height: double.infinity,
-          margin: EdgeInsets.only(top: 40),
-          child: Column(
-            children: <Widget>[
-              Pertanyaan(_apalah[_index]["soal"], _index + 1),
-              Container(
-                margin: EdgeInsets.only(top: 60),
-                padding: EdgeInsets.only(
-                  left: 40,
-                  right: 40,
+    return _index < _apalah.length
+        ? MaterialApp(
+            home: Scaffold(
+                backgroundColor: Colors.red,
+                appBar: AppBar(
+                  backgroundColor: Colors.red,
+                  elevation: 0.0,
                 ),
-                height: 200,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    ...(_apalah[_index]['jawaban'] as List<String>)
-                        .map((jawaban) {
-                      return Jawaban(jawaban, _lanjutPertanyaan);
-                    }),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                body: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  width: double.infinity,
+                  height: double.infinity,
+                  margin: EdgeInsets.only(top: 40),
+                  child: Column(
+                    children: <Widget>[
+                      Pertanyaan(_apalah[_index]["soal"], _index + 1),
+                      Container(
+                        margin: EdgeInsets.only(top: 60),
+                        padding: EdgeInsets.only(
+                          left: 40,
+                          right: 40,
+                        ),
+                        height: 200,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            ...(_apalah[_index]['jawaban']
+                                    as List<Map<String, Object>>)
+                                .map((jawaban) {
+                              return Jawaban(jawaban["salah"],
+                                  () => _lanjutPertanyaan(jawaban["benar"]));
+                            }),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+          )
+        : Hasil(widget.nama, _benar, _resetSoal);
   }
 }
